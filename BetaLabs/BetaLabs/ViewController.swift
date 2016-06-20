c1708524ff4e256f7984ead4de58348e88be7955
Jorge Ovalle <jroz9105@gmail.com>
2016-06-20 15:54:42 -0500

    xcode project created

---
+++ BetaLabs/BetaLabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BetaLabs
+//
+//  Created by Jorge Raul Ovalle Zuleta on 6/20/16.
+//  Copyright © 2016 betalabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
